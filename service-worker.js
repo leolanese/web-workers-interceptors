c5b042a05c41d7853deb92e54ae20b9b0cc0ee85
Leo Lanese <laneseleo@gmail.com>
2023-07-20 12:58:11 +0100

    readme extra info

--- service-worker.js
+++ service-worker.js
@@ -6,8 +6,8 @@ self.addEventListener('fetch', function (event) {
         const cookieHeader = request.headers.get('My-Custom-Cookie');
         const customHeader = request.headers.get('My-Custom-Header');
 
-        console.log('Test Cookie from worker:', cookieHeader);
-        console.log('Test Custom header from worker:', customHeader);
+        console.log('Test Cookie from worker', cookieHeader);
+        console.log('Test Custom header from worker', customHeader);
 
         event.respondWith(fetch(request));
     });
