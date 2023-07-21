c92e835c04226878eeca927d8e3159d7536a77ff
Leo Lanese <laneseleo@gmail.com>
2023-07-21 14:51:53 +0100

    service workeer tyding up

--- interceptor-worker.js
+++ interceptor-worker.js
@@ -34,44 +34,44 @@ self.addEventListener('fetch', event => {
 
     event.respondWith(handleFetchWithSession(modifiedRequest));
 
-    }
-});
-
-const handleFetchWithSession = (request) => {
-    return fetch(request)
-        .then(response => {
+    function handleFetchWithSession(request) {
+        return fetch(request)
+          .then(response => {
             console.log('response status:', response.status);
             
             if (response.status === 403 && !!sessionId) {
-                console.log('403 = credentials invalid');
-
-                // Send WorkerEvent to Main.js to delete sessionId
-                WorkerEvent.waitUntil(
-                    (async () => {
-                        const clientId =
-                            WorkerEvent.resultingClientId !== ""
-                                ? WorkerEvent.resultingClientId
-                                : WorkerEvent.clientId;
-                            const client = await self.clients.get(clientId);
-
-                        client.postMessage('403');
-                    })()
-                );
+              console.log('403 = credentials invalid');
+      
+              // Send event to Main.js to delete sessionId
+              event.waitUntil(
+                (async () => {
+                  const clientId =
+                    event.resultingClientId !== ""
+                        ? event.resultingClientId
+                        : event.clientId;
+                  const client = await self.clients.get(clientId);
+              
+                  console.log("service worker: postMessage = " + sessionId);
+                  client.postMessage('403');
+                })()
+              );
             }
-        
+            
             console.log('Intercepted a fetch request => ', request.url);
-            console.log('Request =>', request);
-            console.log('Response: =>', response);
-
+            console.log('Request:', request);
+            console.log('Response:', response);
+      
             return response;
-    })
-        .catch(() => {
+          })
+          .catch(function() {
             return new Response("Failed to fetch");
-    });
-}
+          });
+    }
 
-// Get sessionID and put it as header
-// if response is 401 --> send event to main.js and remove sessionId if exist
+      // Get sessionID and put it as header
+      // if response is 401 --> send event to main.js and remove sessionId if exist
+  }
+});
 
 onmessage = (e) => {
   console.log('Worker: Message received from main script = ' +  e.data[0]);
--- main.js
+++ main.js
@@ -1,25 +1,30 @@
 if ('serviceWorker' in navigator) {
 
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('interceptor-worker.js', { scope: '/' })
-            .then(function(registration) {
-                console.log('Service worker registered with scope: ', registration.scope);
-            
-                navigator.serviceWorker.addEventListener("message", (event) => {
-                    console.log(`Received message from worker: ${event.data}`);
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('interceptor-worker.js', { scope: '/' })
+        .then(function(registration) {
+            console.log('Service worker registered with scope: ', registration.scope);
+        
+            navigator.serviceWorker.addEventListener("message", (event) => {
+                console.log(`Received message from worker: ${event.data}`);
 
-                    if (event.data === '403') {
-                        // 2
-                        // removed sessionId from localStorage
-                        console.log('delete sessionId from localStorage')
-                        document.cookie = 'sessionId=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-                    }
-                });
-
-            }, function(err) {
-                console.log('ServiceWorker registration failed: ', err);
+                if (event.data === '403') {
+                    // 2
+                    // removed sessionId from localStorage
+                    console.log('delete sessionId from localStorage')
+                    document.cookie = 'sessionId=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+                }
             });
-  });   
+
+        }, err => {
+            console.log('ServiceWorker registration failed: ', err);
+        })
+        .catch(error => {
+            console.error('Error:', error.message); // Handle network or JSON parsing errors
+            return null; // Return null or a default value if necessary
+          });
+  });
+
 }
 
 
